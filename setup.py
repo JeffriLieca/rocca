96cbcc02603ce3ce53531aaecebed8727f2776b8
JeffriLieca <jhandoyo@student.ciputra.ac.id>
2024-01-08 09:00:52 +0700

    Menambahkan algortima

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
 setup(
     name='rocca',
     version='0.1.0',  
     author='Jeffri Lieca H',  
     author_email='handoyojeffri@gmail.com',  
     description='Library Machine Learning Python yang simple dan lengkap ',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/url-projek-anda',  # Ganti dengan URL repositori GitHub Anda
-    packages=find_packages(),
+    url='https://github.com/JeffriLieca/rocca',  # Ganti dengan URL repositori GitHub Anda
+    packages=find_packages(where="rocca"),
     install_requires=[
         'numpy',
         'matplotlib',
@@ -20,6 +27,7 @@ setup(
         'Development Status :: 3 - Alpha',  
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -27,5 +35,7 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
+
+    keywords="simple, apriori, tree, cluster, machine learning, code, python",
     python_requires='>=3.7',  
 )
